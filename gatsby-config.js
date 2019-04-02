4bccf8d70ded44ba72c0d6d74328ee4dd96958c0
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-02 15:34:31 -0400

    Adding svg and sitemap packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.example.com",
+  },
   plugins: [
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-source-graphql",
       options: {
@@ -10,17 +14,25 @@ module.exports = {
         fieldName: "takeshape",
         url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.TAKESHAPE_TOKEN}`,
         },
         // Additional options to pass to node-fetch
         fetchOptions: {},
       },
     },
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: "gatsby-plugin-sass",
       options: {
         implementation: require("node-sass"),
+      }
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/
+        }
       }
     }
   ]
