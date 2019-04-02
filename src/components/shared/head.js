ddb69be413b42dfa5447b06ce8588d253e08d263
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-02 14:48:47 -0400

    Adding meta tags and seo stuff to head

--- src/components/shared/head.js
+++ src/components/shared/head.js
@@ -7,12 +7,25 @@ const Head = (props) => {
   const { takeshape } = data;
   const { siteSettings } = takeshape;
 
+  const title = `${seo.title} | ${siteSettings.siteName}`;
+
   return (
     <Helmet>
       <html lang="en"/>
-      <title>{siteSettings.siteName} | {seo.title}</title>
+      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={seo.description} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:url" content="https://kristenkriens.com/" /> /* TODO: */
+      <meta property="og:site_name" content={siteSettings.siteName} />
+      <meta property="og:image" content={seo.ogImage.path} />
+      <link rel="canonical" href="https://kristenkriens.com/" /> /* TODO: */
+      <link rel="icon" href={siteSettings.favicon.path} type="image/png" />
+      <link rel="apple-touch-icon-precomposed" href={siteSettings.favicon.path} type="image/png" />
     </Helmet>
   )
 };
@@ -22,6 +35,9 @@ export const query = graphql`
     takeshape {
       siteSettings: getSiteSettings {
         siteName
+        favicon {
+          path
+        }
       }
     }
   }
