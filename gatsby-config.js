6b17e0717d8c422ef8edc9adf4b21fcc55f757cf
Kristen Kriens <kristen.kriens@gmail.com>
2019-04-02 15:20:11 -0400

    Added scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,27 @@
 require("dotenv").config();
 
 module.exports = {
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        {
-            resolve: "gatsby-source-graphql",
-            options: {
-                typeName: "TS",
-                fieldName: "takeshape",
-                url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
-                },
-                // Additional options to pass to node-fetch
-                fetchOptions: {},
-            },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "TS",
+        fieldName: "takeshape",
+        url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
         },
-    ]
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("node-sass"),
+      }
+    }
+  ]
 };
